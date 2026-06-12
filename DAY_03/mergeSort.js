fca3784134e71f42b23ee513eebd6a0fc56cb91b
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-06-12 12:54:35 +0530

    implemented merge sort function to merge two sorted arrays

---
+++ DAY_03/mergeSort.js
@@ -0,0 +1,13 @@
+var merge = function(nums1, m, nums2, n) {
+    nums1.splice(m, n, ...nums2);
+    nums1.sort((a, b) => a - b);
+    return nums1
+};
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+
+
+console.log(merge(nums1, m, nums2, n))
